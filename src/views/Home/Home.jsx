49178167c202306e2042e457f384f66081973f68
Peter <pete225848@gmail.com>
2021-12-10 19:59:39 -0800

    passing test for TeamDetails

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,11 +3,9 @@ import { Link, BrowserRouter as Router } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <Router>
-        <h1>Kickball!!!</h1>
-        <Link to="/teams">Go to teams</Link>{' '}
-        <Link to="/players">Go to players</Link>
-      </Router>
+      <h1>Kickball!!!</h1>
+      <Link to="/teams">Go to teams</Link>{' '}
+      <Link to="/players">Go to players</Link>
     </div>
   );
 }
--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
 
 it('should render the homepage', () => {
-  const { container } = render(<Home />);
+  const { container } = render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
   expect(container).toMatchSnapshot();
 });
