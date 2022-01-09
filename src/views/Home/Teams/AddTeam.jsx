2b577d55a37bea9ae137fc81f9dd16def6ab4af4
Peter <pete225848@gmail.com>
2022-01-08 20:52:47 -0800

    created AddTeam component

---
+++ src/views/Home/Teams/AddTeam.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import TeamForm from '../../../components/TeamForm';
+import { createTeam } from '../../../services/teams';
+import { useHistory } from 'react-router-dom';
+
+export default function AddTeam() {
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const res = await createTeam({ name, city, state });
+
+    history.push(`/teams/${res[0].id}`);
+  };
+
+  return (
+    <>
+      <fieldset>
+        <legend>Add a Team</legend>
+        <TeamForm
+          name={name}
+          city={city}
+          state={state}
+          handleSubmit={handleSubmit}
+          setName={setName}
+          setCity={setCity}
+          setState={setState}
+        />
+      </fieldset>
+    </>
+  );
+}
